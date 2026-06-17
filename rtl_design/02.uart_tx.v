3e17a05f86b329cb18093a1f53fcdc9f7b79988f
VaddeBhargav189 <vaddebhargav123456@gmail.com>
2026-06-17 14:41:55 +0530

    Update 02.uart_tx.v

--- rtl_design/02.uart_tx.v
+++ rtl_design/02.uart_tx.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.06.2026 11:52:46
-// Design Name: 
-// Module Name: uart_tx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module uart_tx(
        input clk,
