90bf770f7b7edab755ff1a5cf4f250b2234e1cac
VaddeBhargav189 <vaddebhargav123456@gmail.com>
2026-06-17 14:43:48 +0530

    Update 06.Top_module.v

--- rtl_design/06.Top_module.v
+++ rtl_design/06.Top_module.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.06.2026 10:59:03
-// Design Name: 
-// Module Name: Top_module
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Top_module(
          input clk,
