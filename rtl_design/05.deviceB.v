e91432c44bf1a003279895ebaa5530db0d920363
VaddeBhargav189 <vaddebhargav123456@gmail.com>
2026-06-17 14:43:25 +0530

    Update 05.deviceB.v

--- rtl_design/05.deviceB.v
+++ rtl_design/05.deviceB.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.06.2026 10:55:54
-// Design Name: 
-// Module Name: deviceB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module deviceB(
         input clk,
