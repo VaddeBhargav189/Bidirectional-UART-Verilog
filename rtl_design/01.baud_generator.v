18526e95e7e7276765c742f5fddf3eb5e08e484f
VaddeBhargav189 <vaddebhargav123456@gmail.com>
2026-06-17 14:41:15 +0530

    Update 01.baud_generator.v

--- rtl_design/01.baud_generator.v
+++ rtl_design/01.baud_generator.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.06.2026 11:27:34
-// Design Name: 
-// Module Name: baud_generator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module baud_generator(
          input clk,
