a060e9e9b4a37a266848bfcaa1474c118ce1f2f6
VaddeBhargav189 <vaddebhargav123456@gmail.com>
2026-06-17 14:42:36 +0530

    Update 03.uart_rx.v

--- rtl_design/03.uart_rx.v
+++ rtl_design/03.uart_rx.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.06.2026 11:53:05
-// Design Name: 
-// Module Name: uart_rx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module uart_rx(
       input clk,reset,
